4756cd58cd993fc7aee353f8afdab7673ea2741b
PhoenixChee <cheepohhian@gmail.com>
2023-11-18 15:43:52 +0800

    Update Theme Changer
    Working Finally

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://phoenixchee.github.io',
-  base: '/my-repo',
-})
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
